8435025c8dc7c504b00911a5260c193d65375354
Joel Wurtz <jwurtz@jolicode.com>
2015-09-20 17:43:08 +0200

    Add missing normalization satement for pattern map

--- src/Guesser/Guess/PatternMultipleType.php
+++ src/Guesser/Guess/PatternMultipleType.php
@@ -91,6 +91,50 @@ class PatternMultipleType extends Type
     }
 
     /**
+     * (@inheritDoc}
+     */
+    public function createNormalizationStatement(Context $context, Expr $input)
+    {
+        $valuesVar     = new Expr\Variable($context->getUniqueVariableName('values'));
+        $statements    = [
+            // $values = [];
+            new Expr\Assign($valuesVar, $this->createNormalizationArrayValueStatement()),
+        ];
+
+        $loopValueVar   = new Expr\Variable($context->getUniqueVariableName('value'));
+        $loopKeyVar     = $this->createLoopKeyStatement($context);
+        $loopStatements = [];
+
+        foreach ($this->types as $pattern => $type) {
+            list($typeStatements, $typeOutput) = $type->createNormalizationStatement($context, $loopValueVar);
+            $loopStatements                    = array_merge($loopStatements, [
+                new Stmt\If_(
+                    new Expr\BinaryOp\BooleanAnd(
+                        new Expr\FuncCall(new Name('preg_match'), [
+                            new Arg(new Expr\ConstFetch(new Name("'/".str_replace('/', '\/', $pattern)."/'"))),
+                            new Arg($loopKeyVar)
+                        ]),
+                        $type->createNormalizationConditionStatement($loopValueVar)
+                    ),
+                    [
+                        'stmts' => array_merge($typeStatements, [
+                            new Expr\Assign(new Expr\PropertyFetch($valuesVar, $loopKeyVar), $typeOutput),
+                            new Stmt\Continue_()
+                        ])
+                    ]
+                )
+            ]);
+        }
+
+        $statements[]     = new Stmt\Foreach_($input, $loopValueVar, [
+            'keyVar' => $loopKeyVar,
+            'stmts'  => $loopStatements
+        ]);
+
+        return [$statements, $valuesVar];
+    }
+
+    /**
      * {@inheritDoc}
      */
     protected function createArrayValueStatement()
@@ -104,17 +148,17 @@ class PatternMultipleType extends Type
     /**
      * {@inheritDoc}
      */
-    protected function createLoopKeyStatement(Context $context)
+    protected function createNormalizationArrayValueStatement()
     {
-        return new Expr\Variable($context->getUniqueVariableName('key'));
+        return new Expr\New_(new Name('\stdClass'));
     }
 
     /**
      * {@inheritDoc}
      */
-    protected function createLoopOutputAssignement(Expr $valuesVar, $loopKeyVar)
+    protected function createLoopKeyStatement(Context $context)
     {
-        return new Expr\ArrayDimFetch($valuesVar, $loopKeyVar);
+        return new Expr\Variable($context->getUniqueVariableName('key'));
     }
 }
  
